ef204d2861daa0ae638b446fc22c0c0da6fe6789
dcquijano <dariuscumpio@gmail.com>
2025-03-08 10:01:54 +0800

    done done display

---
+++ my_portfolio/Controllers/MessageController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace my_portfolio.Controllers
+{
+    public class MessageController : Controller
+    {
+        public IActionResult MessageUs()
+        {
+            return View();
+        }
+    }
+}
